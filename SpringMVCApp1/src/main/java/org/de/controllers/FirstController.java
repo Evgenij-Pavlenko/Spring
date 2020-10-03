c91e7a8d9a4e9b93ccc11395db2a6a0525762772
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-04 00:25:05 +0200

    HW - add Controller calculator

--- SpringMVCApp1/src/main/java/org/de/controllers/FirstController.java
+++ SpringMVCApp1/src/main/java/org/de/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.de.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +12,44 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String halloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 //        String name = request.getParameter("name");
 //        String surname = request.getParameter("surname");
-        System.out.println("Hallo, " + name + " " + surname);
+//        System.out.println("Hallo, " + name + " " + surname);
+
+        model.addAttribute("message", "Hallo, " + name + " " + surname);
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+        double res;
+
+        switch (action) {
+            case "+":
+                res = a + b;
+                break;
+            case "-":
+                res = a - b;
+                break;
+            case "*":
+                res = a * b;
+                break;
+            case "/":
+                res = a / (double) b;
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + action);
+        }
+        model.addAttribute("result", a + " " + action + " " + b +
+                " = " + res);
+//        model.addAttribute("result",  res);
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
