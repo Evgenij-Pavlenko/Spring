e96e72dfc3dd521847c6d24f52b90ac16b639a26
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-09 19:46:15 +0200

    People last

--- SpringMVCApp1/src/main/java/org/de/controllers/PeopleController.java
+++ SpringMVCApp1/src/main/java/org/de/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package org.de.controllers;
 
 import org.de.dao.PersonDao;
+import org.de.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -23,7 +22,6 @@ public class PeopleController {
     @GetMapping
     public String index(Model model) {
         model.addAttribute("people", personDao.index());
-//        model.addAttribute("message", "Hello, John");
         return "people/index";
     }
 
@@ -32,6 +30,16 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDao.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+    @PostMapping("")
+    public String create(@ModelAttribute("person") Person person){
+        personDao.save(person);
+        return "redirect:/people";
     }
 
 }
