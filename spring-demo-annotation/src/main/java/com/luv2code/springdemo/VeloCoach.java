79a33d0ecd735d53df0f7d86c905efe7031e5781
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-13 21:46:20 +0200

    Autowired an constructor

--- spring-demo-annotation/src/main/java/com/luv2code/springdemo/VeloCoach.java
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/VeloCoach.java
@@ -9,4 +9,9 @@ public class VeloCoach implements Coach{
     public String getDailyWorkout() {
         return "Ride a bike for 1 hour";
     }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
+    }
 }
