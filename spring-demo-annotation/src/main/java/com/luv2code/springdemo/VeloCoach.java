c8c0ce2fc07db0289f29e5d44341a86244e87189
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-13 19:33:56 +0200

    Config with Annotation. IoC

---
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/VeloCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VeloCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Ride a bike for 1 hour";
+    }
+}
