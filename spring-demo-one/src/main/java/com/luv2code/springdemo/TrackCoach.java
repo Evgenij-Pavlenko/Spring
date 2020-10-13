78042347371790bded39ecae3b818762b43dfc9b
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-13 16:56:11 +0200

    Udemy - LifeCycle and scope

--- spring-demo-one/src/main/java/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/main/java/com/luv2code/springdemo/TrackCoach.java
@@ -1,6 +1,8 @@
 package com.luv2code.springdemo;
 
-public class TrackCoach implements Coach {
+import org.springframework.beans.factory.DisposableBean;
+
+public class TrackCoach implements Coach, DisposableBean {
 
     private FortuneService fortuneService;
     public TrackCoach() {
@@ -18,5 +20,15 @@ public class TrackCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return "Just do it: " + fortuneService.getFortune();
+    }
+
+    //add an init method
+    private void doInit(){
+        System.out.println("TrackCoach: inside method doInit");
+    }
+
+    //add a destroy method
+    public void destroy(){
+        System.out.println("TrackCoach: inside method doDestroy");
     }
 }
