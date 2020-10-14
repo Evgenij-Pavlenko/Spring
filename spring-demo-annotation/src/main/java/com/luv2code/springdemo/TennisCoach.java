9af2c4f386e87e9580f3332ec92b1ddf18f84618
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-14 21:37:35 +0200

    Autowired an set- some-method, field. @Qualify

--- spring-demo-annotation/src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -1,16 +1,37 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TennisCoach implements Coach{
+public class TennisCoach implements Coach {
+
+    //define a field - @Autowired
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
-//    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+    public TennisCoach() {
     }
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+
+    //define a setter method - @Autowired
+//    @Autowired
+//    public void setFortuneService(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+
+    //define a some method - @Autowired
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
