e1ff9b14eb6e1d74307b60814ed4169ea2b5c1d0
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-18 02:01:33 +0200

    CRUD - add Update and Delete

--- SpringMVCApp1/src/main/java/org/de/dao/PersonDao.java
+++ SpringMVCApp1/src/main/java/org/de/dao/PersonDao.java
@@ -19,17 +19,27 @@ public class PersonDao {
         people.add(new Person(++PEOPLE_COUNT, "Name 4"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(int id){
+    public Person show(int id) {
 
-        return people.stream().filter(person->person.getId()==id).findAny().orElse(null);
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
+
