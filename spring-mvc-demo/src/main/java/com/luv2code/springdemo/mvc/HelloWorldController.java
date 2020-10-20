2234c10c7e19c9bbbd30275f096d622f29acb644
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-21 01:24:49 +0200

    add @RequestParam

--- spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -28,10 +28,22 @@ public class HelloWorldController {
     // add data to the model
 
     @RequestMapping("/processFormVersionTwo")
-    public String letsShoutDude(@RequestParam("studentName") String theName, Model model) {
-//        HttpServletRequest request,
+    public String letsShoutDude(HttpServletRequest request, Model model) {
         // read the request parameter from the HTML form
-//        String theName = request.getParameter("studentName");
+        String theName = request.getParameter("studentName");
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+        // create the message
+        String result = "Yo! " + theName;
+        // add message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThre(
+            @RequestParam("studentName") String theName,
+            Model model) {
         // convert the data to all caps
         theName = theName.toUpperCase();
         // create the message
