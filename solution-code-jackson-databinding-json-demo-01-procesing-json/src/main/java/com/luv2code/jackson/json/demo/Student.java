544e289fb4fe75ffc348c14d59b3e62ce7b47174
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-12-27 11:39:32 +0100

    JSON with nested object to Java POJO - ignore properties

--- solution-code-jackson-databinding-json-demo-01-procesing-json/src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ solution-code-jackson-databinding-json-demo-01-procesing-json/src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -1,7 +1,10 @@
 package com.luv2code.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.Arrays;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
