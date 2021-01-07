a4f36ba302e78148df873babe84e29cc83fb2f01
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-08 00:07:58 +0100

    update with JpaRepository - without write cruds methods

--- cruddemo/cruddemo/src/main/java/com/example/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ cruddemo/cruddemo/src/main/java/com/example/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.springboot.cruddemo.service;
 
 import com.example.springboot.cruddemo.dao.EmployeeDAO;
 import com.example.springboot.cruddemo.dao.EmployeeDAOJpaImpl;
+import com.example.springboot.cruddemo.dao.EmployeeRepository;
 import com.example.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,38 +10,48 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
+
+    // when employeeDAOJpaImpl use:
+//    private EmployeeDAO employeeDAO;
+//
+//    @Autowired
+//    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
+//        this.employeeDAO = theEmployeeDAO;
+//    }
 
     @Autowired
-    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        this.employeeRepository = theEmployeeRepository;
     }
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+        if (result.isPresent()){
+            theEmployee = result.get();
+        }
+        return theEmployee;
     }
 
     @Override
-    @Transactional
     public void save(Employee theEmployee) {
-        employeeDAO.save(theEmployee);
+        employeeRepository.save(theEmployee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 }
