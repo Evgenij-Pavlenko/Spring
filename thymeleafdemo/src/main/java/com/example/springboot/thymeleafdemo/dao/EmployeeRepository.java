f4d1bfa49ef53cdbdb89ecc624cb621818d788d9
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-14 17:57:08 +0100

    get sortered list by LastName

--- thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.example.springboot.thymeleafdemo.dao;
 import com.example.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository <Employee, Integer>{
 
     // thet's it ... no need to write any code )
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastName();
 }
